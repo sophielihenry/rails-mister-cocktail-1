48b035f88e860aa231d3d790d9c7f6565ddf11f4
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-04-25 15:02:09 +0100

    delete banner.js error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,3 @@ import 'bootstrap';
 import improveDropdown from '../components/select';
 
 improveDropdown();
-
-
-
-import { loadDynamicBannerText } from 'components/banner';
-loadDynamicBannerText();
